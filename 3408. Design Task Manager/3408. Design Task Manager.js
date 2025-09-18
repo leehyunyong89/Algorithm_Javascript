b4d5b3931255277e1f6e50c0dc123626ae3176e4
Hyunyong Lee <leehyunyong89@naver.com>
2025-09-18 17:45:32 +0900

    Add details

--- 3408. Design Task Manager/3408. Design Task Manager.js
+++ 3408. Design Task Manager/3408. Design Task Manager.js
@@ -54,3 +54,60 @@ At most 2 * 105 calls will be made in total to add, edit, rmv, and execTop metho
 The input is generated such that taskId will be valid.
 
  */
+
+
+
+
+
+
+
+
+/**
+ * @param {number[][]} tasks
+ */
+var TaskManager = function(tasks) {
+    
+};
+
+/** 
+ * @param {number} userId 
+ * @param {number} taskId 
+ * @param {number} priority
+ * @return {void}
+ */
+TaskManager.prototype.add = function(userId, taskId, priority) {
+    
+};
+
+/** 
+ * @param {number} taskId 
+ * @param {number} newPriority
+ * @return {void}
+ */
+TaskManager.prototype.edit = function(taskId, newPriority) {
+    
+};
+
+/** 
+ * @param {number} taskId
+ * @return {void}
+ */
+TaskManager.prototype.rmv = function(taskId) {
+    
+};
+
+/**
+ * @return {number}
+ */
+TaskManager.prototype.execTop = function() {
+    
+};
+
+/** 
+ * Your TaskManager object will be instantiated and called as such:
+ * var obj = new TaskManager(tasks)
+ * obj.add(userId,taskId,priority)
+ * obj.edit(taskId,newPriority)
+ * obj.rmv(taskId)
+ * var param_4 = obj.execTop()
+ */
